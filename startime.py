08e3769a9772e26b0a27597ce41daef42d832f55
mshelur <miles.shelton@richmond.edu>
2025-05-13 12:54:58 -0400

    updated tokenizer

--- startime.py
+++ startime.py
@@ -21,48 +21,50 @@ def get_days_in_month(month: int, year: int) -> bool:
 math_symbols = {'+': add, '-': sub, '*': mult, '/': div}
 days_in_month = [0,31,28,31,30,31,30,31,31,30,31,30,31]
 
-def tokenize_startime(exp: str) -> list[str]:
-    exp = exp.split("/")
-    if len(exp) != 3:
-        raise ValueError("Valid date must be formatted as m/d/y.")
-    
+def tokenize_startime(inp: str) -> list[str]:
     tokens = []
-    for i in range(3):
-        expression = []
-        wildcard_pos = []
-        last = ""
-        grouping_depth = 0
-        for char in exp[i]:
-            if char.isnumeric(): # number
-                if last == "OPERATOR":
+
+    expression = []
+    wildcard_pos = []
+    last = ""
+    grouping_depth = 0
+    for char in inp:
+        if char.isnumeric(): # number
+            if last == "OPERATOR":
+                last = ""
+            last += str(char)
+        elif char in math_symbols: # operator
+            if last and last != "OPERATOR":
+                if last != "": expression.append(last)
+                if char == "/" and grouping_depth == 0:
+                    tokens.append(expression)
                     last = ""
-                last += str(char)
-            elif char in math_symbols: # operator
-                if last and last != "OPERATOR":
-                    if last != "": expression.append(last)
+                    expression = []
+                else:
                     expression.append(char)
                     last = "OPERATOR"
-                elif char == "*":
-                    last = "WILD"
-            elif char in "()": # grouping symbol
-                if char == "(":
-                    grouping_depth += 1
-                else:
-                    grouping_depth -= 1
-                if last != "OPERATOR":
-                    expression.append(last)
-                expression.append(char)
-                last = ""
-            else: 
-                raise ValueError(f"Unknown value '{char}' passed into date '{inp}'.")
-            print(char,expression)
-        if last != "": 
-            expression.append(last)
+            elif char == "*":
+                last = "WILD"
+        elif char in "()": # grouping symbol
+            if char == "(":
+                grouping_depth += 1
+            else:
+                grouping_depth -= 1
+            if last != "OPERATOR":
+                expression.append(last)
+            expression.append(char)
+            last = ""
+        else: # unknown
+            raise ValueError(f"Unknown value '{char}' passed into date '{inp}'.")
+        print(char,expression)
+    if last != "": 
+        expression.append(last)
 
-        if grouping_depth != 0:
-            raise ValueError(f"Grouping symbol(s) missing in {exp[i]}")
-        
-        tokens.append(expression)
+    if grouping_depth != 0:
+        raise ValueError(f"Grouping symbol(s) missing in {exp[i]}")
+    
+    tokens.append(expression)
+
     return tokens
 
 def eval_tree_exp(exp: str) -> int:
